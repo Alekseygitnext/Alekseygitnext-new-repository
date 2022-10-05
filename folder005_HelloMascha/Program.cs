aa32cafefaaff7356e43fcd67fd6d88ab8d7d08b
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-05 20:06:54 +1000

    HelloMascha

---
+++ folder005_HelloMascha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя");
+String username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine ("Ура, это же Маша с Амурлитмаша");
+}
+ else
+{
+    Console.Write("Привет,");
+    Console.Write(username);
+}
